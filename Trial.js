0d7e7c5495a229d0dc90dc0201c03407f9243831
loycevelma <amondiloyce@gmail.com>
2021-04-01 01:02:29 +0300

    first commit

--- Trial.js
+++ Trial.js
@@ -1,74 +1,60 @@
-//function age() {
-// var age = 57;
-//let person = {
-//  firstName: "Juveline",
-// lastName: "cynthia",
-// Age: 26,
-// Height: 267,
-// Weight: 67,
-//fullName: function() {
-// var details = (this.firstName + " " + this.lastName);
-//console.log(details)
-
-// }
-//};
-//console.log(person.lastName)
-//person.fullName()
-
-//fee();
+// this is a function for age
+function age() {
+    var age = 57;
+    let person = {
+        firstName: "Juveline",
+        lastName: "cynthia",
+        Age: 26,
+        Height: 267,
+        Weight: 67,
+        fullName: function() { // this is a method
+            var details = (this.firstName + " " + this.lastName);
+            console.log(details)
 
 
-//function fee() {
+        }
+    };
+    console.log(person.lastName) /
+        person.fullName()
 
-//var a = 4;
-
-//console.log(a)
-
-
-
-//function foo() {
-// var  b = 3;
-//var c = a + b
-//  console.log(b)
-
-//function fuu() {
-
-// var d = 5;
-
-//  var e = a * b * c
-// console.log(d);
-
-// };
-// fuu()
-//};
-//foo()
-//};
+    fee(); // this is Hoisting
 
 
+    function fee() {
+        var a = 4;
+        console.log(a)
 
+        function foo() {
+            var b = 3;
+            var c = a + b
+            console.log(b)
 
+            function fuu() {
 
+                var d = 5;
 
-//var age = 60;
+                var e = a * b * c
+                console.log(d);
 
-//function age() {
+            };
+            fuu()
+        };
+        foo()
+    };
+    fee() //this is nesting
 
-//if (age >= 70) {
-//console.log("A ");
-//}
-//elseif(age >= 60) {
-//console.log("B ");
-//}
-//else if (age >= 50) {
-//console.log("C ");
-//};
-//else
-//if (age <= 49) {
-//console.log("D ");
-//}
-//else if (age >= "E") {
-//console.log("E");
-//}
+    if (age >= 70) {
+        console.log("A ");
+    } else if (age >= 60) {
+        console.log("B ");
+    } else if (age >= 50) {
+        console.log("C ");
+    } else
+    if (age <= 49) {
+        console.log("D ");
+    } else if (age >= "E") {
+        console.log("E"); //this are conditions
+    }
 
-//};
-//age();
+};
+age();
