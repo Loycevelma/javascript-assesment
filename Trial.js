37e25b20b0ca7832732f1016f979448fb3090118
loycevelma <amondiloyce@gmail.com>
2021-04-01 00:33:28 +0300

    first commit

---
+++ Trial.js
@@ -0,0 +1,74 @@
+//function age() {
+// var age = 57;
+//let person = {
+//  firstName: "Juveline",
+// lastName: "cynthia",
+// Age: 26,
+// Height: 267,
+// Weight: 67,
+//fullName: function() {
+// var details = (this.firstName + " " + this.lastName);
+//console.log(details)
+
+// }
+//};
+//console.log(person.lastName)
+//person.fullName()
+
+//fee();
+
+
+//function fee() {
+
+//var a = 4;
+
+//console.log(a)
+
+
+
+//function foo() {
+// var  b = 3;
+//var c = a + b
+//  console.log(b)
+
+//function fuu() {
+
+// var d = 5;
+
+//  var e = a * b * c
+// console.log(d);
+
+// };
+// fuu()
+//};
+//foo()
+//};
+
+
+
+
+
+
+//var age = 60;
+
+//function age() {
+
+//if (age >= 70) {
+//console.log("A ");
+//}
+//elseif(age >= 60) {
+//console.log("B ");
+//}
+//else if (age >= 50) {
+//console.log("C ");
+//};
+//else
+//if (age <= 49) {
+//console.log("D ");
+//}
+//else if (age >= "E") {
+//console.log("E");
+//}
+
+//};
+//age();
